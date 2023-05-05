e22458c1310398217705cf479601bc46f31ebe83
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-05-05 17:34:17 +0200

    Componente header

--- src/components/headerComp.vue
+++ src/components/headerComp.vue
@@ -1,4 +1,5 @@
 <script>
+    import "@fontsource/alike-angular"
     export default {
         name: 'headerComp'
     }
@@ -6,12 +7,35 @@
 
 
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <div class="container p-4 text-center position-relative">
+            <!-- title -->
+            <h1 class="text-white">Yu-Gi-Oh API</h1>
+            <!-- logo centrale -->
+            <div class="position-absolute">
+                <img src="/img/logo-main.png" alt="logo-yu-gi-oh-principale">
+            </div>
+        </div>
+    </header>
 </template>
 
 
 <style lang="scss" scoped>
+    header{
+        background: rgb(121,110,9);
+        background: linear-gradient(90deg, rgba(121,110,9,1) 12%, rgba(121,52,9,1) 65%, rgba(228,46,12,1) 100%);
+    }
+    h1{
+        font-family: "Alike Angular", sans-serif;
+        font-size: 6rem;
+    }
+    img{
+        height: 5em;
+    }
 
+    .position-absolute{
+        bottom: -25%;
+        left: 50%;
+        transform: translate(-50%);
+    }
 </style>
