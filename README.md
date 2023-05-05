f5ec36844529b9afda676a026375327e0f5b0ca2
Antonio Ribezzo <118204207+Antonio-Ribezzo@users.noreply.github.com>
2023-05-05 16:09:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Template base Vite + Vue3
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+  export default{
+    name: 'App'
+  }
+
+</script>
+
+<template>
+
+  <h1 class="text-center">ciao</h1>
+  
+</template>
+
+<style lang="scss">
+  // importo il foglio di stile principale
+  @use './style/main.scss' as *;
+
+</style>
